c6d746c88fd425a2608617fe9add1d5f7c02d210
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-25 11:00:01 +0700

    chore(tool): optimizing tool and add more case for unit test

--- utils/generate_agent.py
+++ utils/generate_agent.py
@@ -3,11 +3,14 @@ import utils.debug as debug
 from config.app import FILEPATHConfig
 from config.user_agent import USERAGENT
 
-def get_user_agent(path=None):
-  path = path or FILEPATHConfig['user_agent']
+used_user_agent = []
 
-  if not path:
+def get_user_agent(path=None, test=False):
+  if test and not path:
     return USERAGENT
+
+  if not test and not path:
+    path = FILEPATHConfig['user_agent']
 
   try:
     file = open(path, 'r')
@@ -24,9 +27,15 @@ def get_user_agent(path=None):
   return USERAGENT + user_agent
 
 def generate_user_agent():
+  if len(used_user_agent) == len(USERAGENT):
+    used_user_agent.clear()
+
   list_agent = get_user_agent()
   length = len(list_agent) - 1
   user_agent = list_agent[random.randint(0, length)]
   debug.log("User agent generated: " + user_agent)
+
+  if user_agent in used_user_agent:
+    return generate_user_agent()
 
   return user_agent
